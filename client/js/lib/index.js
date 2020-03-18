e6f3935028c9bd4b46829fc5c03a74d567cc81d9
Andressa <dressalsl97@gmail.com>
2020-03-18 15:11:52 -0300

    first commit

---
+++ client/js/lib/index.js
@@ -0,0 +1,13 @@
+var campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+console.log(campos);
+
+document.querySelector('.form').addEventListener('submit', function(event){
+
+
+    alert('oii');
+});
